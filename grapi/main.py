0ae93748e2fb58765cf06e51348c28d60c9351b6
pcooper010 <patrick.cooper@pwc.com>
2020-04-25 12:44:50 -0500

    Banter Bot (Birthday Bot) - v1.0.0

--- grapi/main.py
+++ grapi/main.py
@@ -4,14 +4,14 @@ import grapi.get as get
 import grapi.post as post
 
 # Send message
-# post.post_message('"Test - disregard" - Cheeky Banter IT', config.config['BOT']['Sillip Phasser'].get())
+# post.post_message('Test', config.config['BOT']['Birthday Bot'].get())
 
 # Create bot
-# avi_url = "https://fcit.usf.edu/matrix/wp-content/uploads/2016/12/Robot-04-C.png"
-# post.create_bot("59458329", 'Birthday Bot', avatar_url=avi_url)
+# avi_url = ""
+# post.create_bot("", 'Birthday Bot', avatar_url=avi_url)
 
 # Delete bot
-# delete.delete_bot(config.config['BOT']['Sillip Phasser'].get())
+# delete.delete_bot(config.config['BOT']['Birthday Bot'].get())
 
 # Get all bots
 # response = get.get_all()
